a2cbc5cda8a61100dda68eac17c4f82846331eba
Yeamin Foysal <yeaminfoysal14@gmail.com>
2025-02-19 10:26:35 +0600

    changed

--- src/features/counter/counterView.jsx
+++ src/features/counter/counterView.jsx
@@ -11,7 +11,7 @@ const counterView = () => {
             <h2 className='text-xl mb-6 font-semibold'>Counte: {count}</h2>
             <button className=' border px-3 py-1 bg-blue-300 rounded-lg mr-2' onClick={() => {dispatch(increment())}}>Increment</button>
             <button className=' border px-3 py-1 bg-blue-300 rounded-lg' onClick={() => {dispatch(decrement())}}>Decrement</button>
-            <button className=' border px-3 py-1 bg-blue-300 rounded-lg ml-2' onClick={() => {dispatch(reset())}}>Reset</button>
+            <button className=' border px-3 py-1 bg-blue-300 rounded-lg ml-2' onClick={() => {dispatch(reset())}}>Reset Number</button>
             <button className=' border px-3 py-1 bg-blue-300 rounded-lg ml-2' onClick={() => {dispatch(increaseByNumber(5))}}>Increase by 5 Number</button>
         </div>
     );
